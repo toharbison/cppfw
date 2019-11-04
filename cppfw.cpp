b26b6716a4160efb2adac12daec404b91e24e100
Teddy Harbison <toharbison@gmail.com>
2019-11-04 12:00:05 -0600

    Started command execution function

--- cppfw.cpp
+++ cppfw.cpp
@@ -14,9 +14,9 @@ struct Rule {
 struct Iptable {
   string* m_chains;
   Rule** m_rules;
-};
+} table;
 
-string* parseCommands(string input);
+void exCommands(string[] input);
 void insert(Rule rule, int ruleNum);
 void replace(Rule rule, Rule rule);
 void replace(Rule rule, int ruleNum);
@@ -33,4 +33,27 @@ void default(string chain, string target);
 int main(int argc, char[]* argv){
   
   return 0;
+}
+
+void exCommands(string[] input){
+  string[] valid = { "default", "allow", "drop", "return", "log", "delete", "replace", "help"};
+  if(input[0] == valid[0]){
+    //TODO
+  }else if(input[0] == valid[1]){
+    //TODO
+  }else if(input[0] == valid[2]){
+    //TODO
+  }else if(input[0] == valid[3]){
+    //TODO
+  }else if(input[0] == valid[4]){
+    //TODO
+  }else if(input[0] == valid[5]){
+    //TODO
+  }else if(input[0] == valid[6]){
+    //TODO
+  }else if(input[0] == valid[7]){
+    //TODO
+  }else{
+    //TODO
+  }
 }
