f6124d36fc1256d9d25cca20d593632536e8279a
Teddy Harbison <toharbison@gmail.com>
2021-06-09 14:12:25 -0500

    Restarted project. Created Firewall class to control rules and get logs

---
+++ firewall.h
@@ -0,0 +1,45 @@
+#ifndef FIREWALL_H
+#define FIREWALL_H
+
+#include <string>
+#include <vector>
+#include <libiptc/libiptc.h>
+#include <xtables.h>
+
+
+class Firewall{
+  public:
+
+  // Constructor
+  Firewall();
+
+  // Deconstructor
+  ~Firewall();
+
+  /* Adds rule to firewall
+   * "dstIp" is destination ip of packet
+   * "srcIp" is source ip of packet
+   * "iFace" is input interface
+   * "oFace" is output interface
+   * "proto" is protocol of packet
+   * "matches" is a vector of rules to match against packet
+   * "target" is the target of packet
+   */
+  void addRule(std::string* dstIp, std::string* srcIp, std::string* iFace, std::string* oFace, 
+      std::string* proto, std::vector<std::string>* matches, std::string target); 
+
+  /* Checks logs and returns std::string of recent log messages
+   * "lines" number of messages to return
+   */
+  std::string checkLogs(int lines);
+
+  private:
+  
+  xtc_handle* rules;
+  std::string ruleFile;
+
+
+};
+
+#endif
+
